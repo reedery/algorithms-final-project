5f961491f7d1be39c8513433222e285b809a5229
nashdb <nashdb@bc.edu>
2016-11-09 23:54:51 -0500

    Lame Square Function

--- ConvertImage.py
+++ ConvertImage.py
@@ -1,17 +1,14 @@
 """
-Super Basic way to make squares and save them in a text file
-Makes random squares, which we can later shift and rotate
-
-By: Danielle
+Converting an Image to Black and White Array
 
 """
-import numpy as np
-import random
+import numpy 
 
 ## Size is 128 x 128
 
 def returnRow(totalSize, onesStart, onesEnd):
 	row = []
+	
 	for i in range(totalSize):
 		if(i >= onesStart and i < onesEnd):
 			row.append(1)
@@ -23,34 +20,37 @@ def returnRow(totalSize, onesStart, onesEnd):
 def makeArray(rowSize, colSize, rStart, rEnd, cStart, cEnd):	
 	
 	doubleList = []
+
 	for i in range(rStart):
 		doubleList.append(returnRow(rowSize, -1, -1))
+
 	for i in range(rEnd - rStart): 
 		doubleList.append(returnRow(rowSize, cStart, cEnd))
+	
 	for i in range(colSize - rEnd):
 		doubleList.append(returnRow(rowSize, -1, -1))
 	
-	arg = np.array(doubleList)
+	#print doubleList
+
+	arg = numpy.array(doubleList)
 	print arg
 	return arg
 	
 delim = "\n#\n"
 file = open("/Users/danielleNash/Desktop/AlgsPics/output.txt", "w")
 
-height = 16
-width = 16
+file.write(str(makeArray(16, 16, 4, 12, 4, 12)))
 
-for i in range(15):
-	rStart = random.randint(0, 14)
-	rEnd = random.randint(rStart+2, 16)
-	cStart = random.randint(0, 14)
-	cEnd = random.randint(cStart+2, 16)
-	
-	#array1 = makeArray(height, width, rStart, rEnd, cStart, cEnd)
-	#outputFile = 'output_main' + str(i) + ".txt"
-	file.write(str(makeArray(height, width, rStart, rEnd, cStart, cEnd)))
-	file.write(delim)
+file.write(delim)
 
-	
-
+file.write(str(makeArray(16, 16, 2, 10, 4, 6)))
+file.write(delim)
+file.write(str(makeArray(16, 16, 3, 10, 1, 9)))
+file.write(delim)
+file.write(str(makeArray(16, 16, 3, 10, 1, 15)))
+file.write(delim)
+file.write(str(makeArray(16, 16, 3, 10, 1, 15)))
+file.write(delim)
+file.write(str(makeArray(16, 16, 2, 14, 8, 12)))
+file.write(delim)
 file.close()
